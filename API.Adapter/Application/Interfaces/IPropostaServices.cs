bdfeea2d4edce0c39ef5624a95803308bd65789d
adrianojcgit <48298141+adrianojcgit@users.noreply.github.com>
2023-12-15 19:01:32 -0300

    Delete API.Adapter directory

--- API.Adapter/Application/Interfaces/IPropostaServices.cs
+++
@@ -1,9 +0,0 @@
-﻿using API.Adapter.Application.DTO;
-
-namespace API.Adapter.Application.Interfaces
-{
-    public interface IPropostaServices
-    {
-        Task<bool> AdicionarProposta(PropostaBaseDto propostaDto);
-    }
-}
--- API.Adapter/Domain/Interfaces/IPropostaRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using API.Adapter.Domain.Entities;
-
-namespace API.Adapter.Domain.Interfaces
-{
-    public interface IPropostaRepository
-    {
-        Task<bool> AdicionarProposta(PropostaBase proposta);
-    }
-}
